e6da59572330f6e5dd1c9cbdc5f8a7c49c944794
joezanini <jzanini@ucsc.edu>
2020-05-15 14:36:51 -0700

    flowtables added

--- lab3controller.py
+++ lab3controller.py
@@ -4,6 +4,8 @@
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
+import pox.lib.packet as pkt
+import time
 
 log = core.getLogger()
 
@@ -22,7 +24,39 @@ class Firewall (object):
 
   def do_firewall (self, packet, packet_in):
     # The code in here will be executed for every packet.
-    print "Example Code."
+
+    msg = of.ofp_flow_mod()
+    msg.match.dl_type = pkt.ARP_TYPE
+    port = self.macToPort[packet.dst]
+
+    #https://stackoverflow.com/questions/54407743
+    #/sdn-pox-controller-arp-type-ip-type-but-no-icmp-type
+    if packet.type == pkt.ARP_TYPE :
+    	log.debug("installing flow for %s.%i -> %s.%i" %
+                  (packet.src, packet.port, packet.dst, port))
+    	msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet, packet_in.port)
+        msg.idle_timeout = 10
+        msg.hard_timeout = 30
+        msg.actions.append(of.ofp_action_output(port = port))
+        msg.data = packet_in 
+        self.connection.send(msg)
+    elif packet.type == pkt.IP_TYPE :
+    	ip_packet = packet.payload
+    	if ip_packet.protocol == pkt.ICMP_PROTOCOL :
+    		log.debug("installing flow for %s.%i -> %s.%i" %
+                  (packet.src, event.port, packet.dst, port))
+    		msg = of.ofp_flow_mod()
+        	msg.match = of.ofp_match.from_packet(packet, packet.port)
+        	msg.idle_timeout = 10
+        	msg.hard_timeout = 30
+        	msg.actions.append(of.ofp_action_output(port = port))
+        	msg.data = packet_in 
+        	self.connection.send(msg)
+    else :
+		self.connection.send(msg)        
+
+
 
   def _handle_PacketIn (self, event):
     """
